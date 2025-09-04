0d4e5fe7e67d474babfb1b2d4ae669bbbb17b04a
Gautheron Olivia <oxtramag@gmail.com>
2025-09-04 21:08:19 +0200

    :bug: fix:bug deploy

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
+  const isProduction = api.env("production");
+
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxRuntime: "automatic",
+          jsxImportSource: "react",
+        },
+      ],
+    ],
+    plugins: isProduction ? [] : [],
   };
 };
