4820c06ed4258b861ebd31134367960ee9737b5a
Gautheron Olivia <oxtramag@gmail.com>
2025-09-04 10:44:22 +0200

    fix:bug

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
+
+  // Configuration différente selon l'environnement
+  const isWeb = api.caller && api.caller.name === "babel-loader";
+
+  const presets = ["babel-preset-expo"];
+
+  // Ajouter les presets React et TypeScript pour le web
+  if (isWeb) {
+    presets.push("@babel/preset-react", "@babel/preset-typescript");
+  }
+
   return {
-    presets: ["babel-preset-expo"],
+    presets: presets,
   };
 };
